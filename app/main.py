a9090fa0963d82765b4a26af11ed535937b6f316
SM41b <mandalsharanya41b@gmail.com>
2026-09-08 16:41:40 +0530

    Add Prometheus monitoring

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI(title="DevOps Assessment API")
+
+Instrumentator().instrument(app).expose(app)
 
 
 @app.get("/")
